51a5199295eb5c3acb79b12b7847d45b13cdf37a
AlecBlyth <63587533+AlecBlyth@users.noreply.github.com>
2020-04-22 16:18:22 +0100

    Update BunnyCorpGame.java

--- src/com/company/BunnyCorpGame.java
+++ src/com/company/BunnyCorpGame.java
@@ -1,28 +1,30 @@
 package com.company;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
-public class BunnyCorpGame {
+class BunnyCorpGame {
 
-    Scanner in = new Scanner(System.in);
+    private Scanner in = new Scanner(System.in);
     private static final String fileName = "src\\Workshop\\sample.txt";
-    Fighter[] myFighter = new Fighter[10];
-    Fighter tempFighter = null;
+    private Fighter[] myFighter = new Fighter[10];
+    private Fighter tempFighter = null;
+    private Fighter secondFighter = null;
 
-    int menuItem;
+    private int menuItem;
 
-    boolean quit = false;
-    boolean success = false;
-    boolean found = false;
+    private boolean quit = false;
+    private boolean found = false;
 
 
-    public BunnyCorpGame() throws IOException {
+    BunnyCorpGame() throws IOException {
 
-        Fighter f1 = new Fighter(1000, "Alec", "Blyth", "United Kingdom", "Fife", "Kickboxing",
+        Fighter f1 = new Fighter(1000, "Alec", "Blyth", "United Kingdom", "Kirkcaldy", "Kickboxing",
                 23, 60, 168, 95, 80, 75, 87, 70);
-        Fighter f2 = new Fighter(1001, "Scott", "Fergerson", "United Kingdom", "Fife", "Submission",
+        Fighter f2 = new Fighter(1001, "Scott", "Fergerson", "United Kingdom", "Dunfermline", "Submission",
                 19, 60, 168, 75, 70, 75, 90, 90);
 
 
@@ -32,28 +34,22 @@ public class BunnyCorpGame {
     }
 
     private int nextFighterID = 1002;
-    int fightStrength = 35;
-    int fightSpeed = 35;
-    int fightEndurance = 35;
-    int fightSubDef = 35;
-    int fightSubOff = 35;
+    private String newline = System.getProperty("line.separator");
 
-    String newline = System.getProperty("line.separator");
-    
-     private boolean gameMaths(double probabilityOfSuccess){ //Calcuates a random chance for fights based on input value.
-        return Math.random() > probabilityOfSuccess;
+    private boolean gameMaths(double probabilityOfSuccess) { //Calculates a random chance for fights based on input value.
+        return new Random().nextDouble() <= probabilityOfSuccess;
     }
 
-    private void startGame() throws IOException{
+    private void startGame() throws IOException {
 
         do {
             System.out.println("|==========================================================|");
-            System.out.println("|                                                       BunnyCorp MMA Game                                                   |");
+            System.out.println("|                   BunnyCorp MMA Game                     |");
             System.out.println("|==========================================================|");
-            System.out.println("|     1. Add Fighters                                                                                                                   |");
-            System.out.println("|     2. View Fighters                                                                                                                  |");
-            System.out.println("|     3. Play Game                                                                                                                       |");
-            System.out.println("|     0. Exit Game                                                                                                                        |");
+            System.out.println("|     1. Add Fighters                                      |");
+            System.out.println("|     2. View Fighters                                     |");
+            System.out.println("|     3. Play Game                                         |");
+            System.out.println("|     0. Exit Game                                         |");
             System.out.println("|==========================================================|");
 
             menuItem = in.nextInt();
@@ -95,19 +91,25 @@ public class BunnyCorpGame {
                                 System.out.println("Enter their height: ");
                                 int fightHeight = in.nextInt();
                                 writer.write(fightHeight + "|");
+                                int fightStrength = 35;
+                                int fightSpeed = 35;
+                                int fightEndurance = 35;
+                                int fightSubDef = 35;
+                                int fightSubOff = 35;
                                 Fighter f = new Fighter(nextFighterID++, firName, secName, Nation, fightOut, fightStyle, fightAge, fightWeight, fightHeight, fightStrength, fightSpeed, fightEndurance, fightSubDef, fightSubOff);
                                 myFighter[i] = f;
                                 writer.newLine();
                                 break;
                             }
                         }
+                        boolean success = false;
                         if (!success) {
                             System.out.println("Fighter Created");
                             writer.close();
                         }
                         break;
 
-                    } catch (IOException e){
+                    } catch (IOException e) {
                         System.out.println(e);
                     }
                 case 2:
@@ -177,7 +179,7 @@ public class BunnyCorpGame {
                     }
                     if (!found) {
                         System.out.println("This fighter does not exist!");
-                        x = 0; //fixed issue where counter continued to increment 
+                        x = 0; //fixed issue where counter continued to increment
                     }
                     break;
 
@@ -190,7 +192,7 @@ public class BunnyCorpGame {
                         break;
                     }
                 case 3:
-                    System.out.println("Test Game");
+                    fightMenu();
                     break;
                 case 4:
                     startGame();
@@ -200,7 +202,7 @@ public class BunnyCorpGame {
         } while (!quit);
     }
 
-private void TrainFighter() throws IOException {
+    private void TrainFighter() throws IOException {
         do {
             System.out.println("|==========================================================|");
             System.out.println("|                    BunnyCorp MMA Game                    |");
@@ -219,15 +221,15 @@ private void TrainFighter() throws IOException {
             switch (menuItem) {
                 case 1:
                     int strength = tempFighter.getStrength();
-                            if (tempFighter.getStrength() == 100) {
-                                System.out.println("Your fighter has maximum Strength");
-                            } else {
-                                for (Fighter aMyFighter : myFighter) {
-                                    if (aMyFighter == null) {
-                                        tempFighter.setStrength(strength + 1);
-                                        break;
-                                    }
-                                }
+                    if (tempFighter.getStrength() == 100) {
+                        System.out.println("Your fighter has maximum Strength");
+                    } else {
+                        for (Fighter aMyFighter : myFighter) {
+                            if (aMyFighter == null) {
+                                tempFighter.setStrength(strength + 1);
+                                break;
+                            }
+                        }
                     }
                     break;
                 case 2:
@@ -259,11 +261,11 @@ private void TrainFighter() throws IOException {
 
                 case 4:
                     int subOff = tempFighter.getSubmissionOffence();
-                    if(tempFighter.getSubmissionOffence() == 100){
+                    if (tempFighter.getSubmissionOffence() == 100) {
                         System.out.print("Your fighter has maximum Submission Offence");
                     } else {
-                        for (Fighter aMyFighter : myFighter){
-                            if(aMyFighter == null){
+                        for (Fighter aMyFighter : myFighter) {
+                            if (aMyFighter == null) {
                                 tempFighter.setSubmissionOffence(subOff + 1);
                             }
                         }
@@ -271,21 +273,21 @@ private void TrainFighter() throws IOException {
                     break;
                 case 5:
                     int subDef = tempFighter.getSubmissionDefence();
-                    if(tempFighter.getSubmissionDefence() == 100){
+                    if (tempFighter.getSubmissionDefence() == 100) {
                         System.out.print("Your fighter has maximum Submission Offence");
                     } else {
-                        for (Fighter aMyFighter : myFighter){
-                            if(aMyFighter == null){
+                        for (Fighter aMyFighter : myFighter) {
+                            if (aMyFighter == null) {
                                 tempFighter.setSubmissionDefence(subDef + 1);
                             }
                         }
                     }
                     break;
                 case 6:
-                    System.out.println("Fighter:   "+ tempFighter.getName());
-                    System.out.println("Strength:  "+ tempFighter.getStrength());
-                    System.out.println("Speed:     "+ tempFighter.getSpeed());
-                    System.out.println("Endurance: "+ tempFighter.getEndurance());
+                    System.out.println("Fighter:   " + tempFighter.getName());
+                    System.out.println("Strength:  " + tempFighter.getStrength());
+                    System.out.println("Speed:     " + tempFighter.getSpeed());
+                    System.out.println("Endurance: " + tempFighter.getEndurance());
                     System.out.println("Submission Offence: " + tempFighter.getSubmissionOffence());
                     System.out.println("Submission Defence: " + tempFighter.getSubmissionDefence());
                     break;
@@ -298,10 +300,15 @@ private void TrainFighter() throws IOException {
         } while (!quit);
     }
 
-   private void fightMenu() {
+    private void fightMenu() throws IOException {
 
         int x = 0;
-        
+
+        if (tempFighter == null) {
+            System.out.println("Select a fighter!");
+            gameMenu();
+        }
+
         System.out.println("        Pick your opponent        " + newline);
         for (Fighter aMyFighter : myFighter) {
             if (aMyFighter == null)
@@ -313,7 +320,7 @@ private void TrainFighter() throws IOException {
         for (Fighter aMyFighter : myFighter) {
             if (aMyFighter != null) {
                 if (aMyFighter.getID() == fighterID) {
-                    if(fighterID == tempFighter.getID()){
+                    if (fighterID == tempFighter.getID()) {
                         System.out.println("You cannot select the same fighter!");
                         fightMenu();
                     } else {
@@ -326,6 +333,144 @@ private void TrainFighter() throws IOException {
         if (!found) {
             System.out.println("This fighter does not exist!");
             x = 0; //fixed issue where counter continued to increment
+        }
+
+    }
+
+    private void fightMethod() throws IOException {
+
+        int wins_1 = tempFighter.getWins();
+        int wins_2 = secondFighter.getWins();
+
+        int losses_1 = tempFighter.getLose();
+        int losses_2 = secondFighter.getLose();
+
+        int str_1 = tempFighter.getStrength();
+        int str_2 = secondFighter.getStrength();
+
+        int end_1 = tempFighter.getEndurance();
+        int end_2 = secondFighter.getEndurance();
+
+        int sub_O1 = tempFighter.getSubmissionOffence();
+        int sub_O2 = secondFighter.getSubmissionOffence();
+
+        int sub_Def1 = tempFighter.getSubmissionDefence();
+        int sub_Def2 = secondFighter.getSubmissionDefence();
+
+        int hp_1 = 100 + end_1;
+        int hp_2 = 100 + end_2;
+
+        int ap_1 = str_1 + 100 / 10;
+        int ap_2 = str_2 + 100 / 10;
+
+        boolean fightOver = false;
+        int fightCount = 5;
+
+
+        while (fightCount != 0 && hp_1 > 0 && hp_2 > 0 && !fightOver) {
+
+            if (gameMaths(0.10)) {
+                if (sub_O1 > sub_Def2) {
+                    if (gameMaths(0.25)) {
+                        System.out.println(tempFighter.getName() + " takes down " + secondFighter.getName());
+                        hp_2 = hp_2 - sub_O1;
+                    }
+                } else {
+                    if (gameMaths(0.10)) {
+                        System.out.println(tempFighter.getName() + " takes down " + secondFighter.getName());
+                        hp_2 = hp_2 - sub_O1;
+                    }
+                }
+                if (hp_2 <= 0) {
+                    System.out.println(secondFighter.getName() + " taps out!");
+                    tempFighter.setWinRate(wins_1 + 1);
+                    secondFighter.setLoseRate(losses_2 + 2);
+                    fightOver = true;
+                    gameMenu();
+                }
+            } else if (!gameMaths(0.10)) {
+                if (sub_O2 > sub_Def1) {
+                    if (gameMaths(0.25)) {
+                        System.out.println(tempFighter.getName() + " takes down " + secondFighter.getName());
+                        hp_1 -= sub_O2;
+                    }
+                } else {
+                    if (gameMaths(0.10)) {
+                        System.out.println(tempFighter.getName() + " takes down " + secondFighter.getName());
+                        hp_1 -= sub_O2;
+                    }
+                }
+                if (hp_1 <= 0) {
+                    System.out.println(secondFighter.getName() + " taps out!");
+                    tempFighter.setWinRate(losses_1 + 1);
+                    secondFighter.setLoseRate(wins_2 + 2);
+                    fightOver = true;
+                    gameMenu();
+                }
+            }
+
+            while (!(!gameMaths(0.25) || fightOver)) {
+                System.out.println(tempFighter.getName() + " hits " + secondFighter.getName());
+                hp_2 -= ap_1;
+                System.out.println(hp_2);
+
+                if (hp_2 <= 0 && !fightOver) {
+                    System.out.println(tempFighter.getName() + " KO/TKOs " + secondFighter.getName());
+                    tempFighter.setWinRate(wins_1 + 1);
+                    secondFighter.setLoseRate(losses_2 + 1);
+                    fightOver = true;
+                    gameMenu();
+                } else if (hp_1 <= 0 && !fightOver) {
+                    System.out.println(secondFighter.getName() + " KO/TKOs " + tempFighter.getName());
+                    secondFighter.setWinRate(wins_2 + 1);
+                    tempFighter.setLoseRate(losses_1 + 1);
+                    fightOver = true;
+                    gameMenu();
+                }
+            }
+            if (!gameMaths(0.25)) {
+                if (gameMaths(0.25)) {
+                    System.out.println(secondFighter.getName() + " hits " + tempFighter.getName());
+                    hp_1 -= ap_2;
+                    System.out.println(hp_1);
+                    if (hp_2 <= 0 && !fightOver) {
+                        System.out.println(tempFighter.getName() + " KO/TKOs " + secondFighter.getName());
+                        tempFighter.setWinRate(wins_1 + 1);
+                        secondFighter.setLoseRate(losses_2 + 1);
+                        fightOver = true;
+                        gameMenu();
+                    } else if (hp_1 <= 0 && !fightOver) {
+                        System.out.println(secondFighter.getName() + " KO/TKOs " + tempFighter.getName());
+                        secondFighter.setWinRate(wins_2 + 1);
+                        tempFighter.setLoseRate(losses_1 + 1);
+                        fightOver = true;
+                        gameMenu();
+                    }
+                } else {
+                    System.out.println("Both fighters missed");
+                }
+            }
+            fightCount--;
+
+            if (fightCount == 0 && !fightOver) {
+                if ((hp_2 - end_2) < (hp_1 - end_1)) {
+                    System.out.println(tempFighter.getName() + " wins by decision! ");
+                    tempFighter.setWinRate(wins_1 + 1);
+                    secondFighter.setLoseRate(losses_2 + 1);
+                    fightOver = true;
+                    gameMenu();
+                } else if ((hp_1 - end_1) < (hp_2 - end_2)) {
+                    System.out.println(secondFighter.getName() + " wins by decision! ");
+                    secondFighter.setWinRate(wins_2 + 1);
+                    tempFighter.setLoseRate(losses_1 + 1);
+                    fightOver = true;
+                    gameMenu();
+                } else if (hp_1 - end_1 == hp_2 - end_2) {
+                    System.out.println("No Contest");
+                    fightOver = true;
+                    gameMenu();
+                }
+            }
         }
     }
 }
